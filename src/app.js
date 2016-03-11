be3457a4c9be4ed186c511a70ae1eb84d596d80b
SwagMal <bjarturhafthors@hotmail.com>
2016-03-11 17:43:33 +0000

    Smá bilað en progress

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@
 angular.module("project3App", ["ngRoute", "ui.bootstrap", "sharedServices", "pascalprecht.translate"])
 .config(function ($routeProvider, $translateProvider) {
 	$routeProvider.when("/", {
-		controller: "ProductController",
-		templateUrl: "components/product/index.html"
+		controller: "SellersController",
+		templateUrl: "components/sellers/index.html"
 	});
 /*
 	$translateProvider.useStaticFilesLoader({
