542d6d956a4146e9e68f45d174b339d45eb0b404
SwagMal <bjarturhafthors@hotmail.com>
2016-03-12 19:18:07 +0000

    Removed old junk files

--- src/components/product/ProductController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-angular.module("project3App").controller("ProductController",
-function ProductController($scope, $location, AppResource, $routeParams) {
-	// TODO: load data from AppResource! Also, add other methods, such as to
-	// add/update product etc.
-	
-	
-});
