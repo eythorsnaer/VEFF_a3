33a4e5c54fdadbd91052fe7f162d3938f50663ed
SwagMal <bjarturhafthors@hotmail.com>
2016-03-12 18:52:47 +0000

    Repeat virkar, en repeatar ekki rett stuff

--- src/components/product/ProductController.js
+++ src/components/product/ProductController.js
@@ -5,13 +5,5 @@ function ProductController($scope, $location, AppResource, $routeParams) {
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update product etc.
 	
-	$scope.text = "Products";
-	$scope.products = [];
-	var sellerID = parseInt($routeParams.id);
-
-	AppResource.getSellerProducts(sellerID).success(function(sellerProducts){
-		$scope.products = sellerProducts;
-		console.log('id: ', sellerID);
-		console.log($scope.products);
-	});
+	
 });
