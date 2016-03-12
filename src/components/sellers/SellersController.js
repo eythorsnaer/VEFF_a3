8c7700142bd2ba7da8fc0e3485092c5661370673
eythorsnaer <eythorsnaer@gmail.com>
2016-03-12 15:33:13 +0000

    edit Seller

--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -5,13 +5,7 @@ function SellersController($scope, AppResource, centrisNotify, SellerDlg) {
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
 	$scope.isLoading = true;
-
 	$scope.sellers = [];
-	$scope.selectedSeller = {
-		name: "",
-		category: "",
-		imagePath: ""
-	};
 
 	AppResource.getSellers().success(function(sellers) {
 		$scope.sellers = sellers;
@@ -24,26 +18,20 @@ function SellersController($scope, AppResource, centrisNotify, SellerDlg) {
 		
 		SellerDlg.show().then(function(seller) {
 			AppResource.addSeller(seller).success(function(seller) {
-				//$scope.sellers.push(seller);
 				centrisNotify.success("sellers.Messages.SaveSucceeded"); //, "sellers.Ok");
-				//console.log("save success");
 			}).error(function() {
-				//TODO: error message
 				centrisNotify.error("sellers.Messages.SaveFailed"); //, "sellers.Fail");
-				//console.log("save error");
 			});
 		});
 	};
 
 	$scope.onEditSeller = function onEditSeller(selectedSeller) {
 
-		SellerDlg.show().then(function(seller) {
-			AppResource.updateSeller(seller.id, seller).success(function(seller) {
+		SellerDlg.show(selectedSeller).then(function(seller) {
+			AppResource.updateSeller(selectedSeller.id, seller).success(function(seller) {
 				centrisNotify.success("sellers.Messages.UpadateSucceeded");
-				//console.log("update success");
 			}).error(function() {
 				centrisNotify.error("sellers.Messages.UpdateFailed");
-				//console.log("update error");
 			});
 		});
 	};
