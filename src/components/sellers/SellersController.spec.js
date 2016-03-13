26c2237e2a953abed661feed3e34348983faf482
eythorsnaer <eythorsnaer@gmail.com>
2016-03-13 18:11:51 +0000

    unit me

--- src/components/sellers/SellersController.spec.js
+++ src/components/sellers/SellersController.spec.js
@@ -4,22 +4,40 @@ describe("SellersController should be unit tested here", function() {
 	// TODO: add beforeEach/describe/it/etc. functions as appropriate!
 	beforeEach(module("project3App"));
 
-    var SellersController, scope;
+    var SellersController, scope, appResource, sellerDlg;
+    var mockLocation = {
+        path: function(p) {
+        }
+    };
 
 	describe("SellersController", function() {
 
-        beforeEach(inject(function ($rootScope, AppResource, centrisNotify, $controller) {
+        beforeEach(inject(function ($rootScope, AppResource, centrisNotify, SellerDlg) {
         	scope = $rootScope.$new();
+            appResource = AppResource;
+            sellerDlg = SellerDlg;
+
+            spyOn(resource, 'getSellers').and.callThrough();
+            spyOn(resource, 'addSeller').and.callThrough();
+            spyOn(resource, 'updateSeller').and.callThrough();
+            spyOn(sellerDlg, 'show').and.callThrough();
+            spyOn(mockLocation, "path");
 
         	SellersController = $controller('SellersController', {
                 $scope: scope,
-                
+                $location: mocLocation,
+                AppResource: appResource,
+                sellerDlg: sellerDlg
             });
         }));
     });
 
+    it("says SellersController is defined", function() {
+        expect(SellersController).toBeDefined();
+    });
+
     it('says testVar is ', function () {
-        expect(scope.testVar === "sjomli");
+        expect(scope.testVar).toEqual("sjomli");
     });
 
 });
