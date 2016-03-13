28a1d1d3c57ea7bb9412b0f16462dbb2795601fa
eythorsnaer <eythorsnaer@gmail.com>
2016-03-13 18:24:17 +0000

    unit me tests

--- src/components/sellers/SellersController.spec.js
+++ src/components/sellers/SellersController.spec.js
@@ -5,39 +5,29 @@ describe("SellersController should be unit tested here", function() {
 	beforeEach(module("project3App"));
 
     var SellersController, scope, appResource, sellerDlg;
-    var mockLocation = {
-        path: function(p) {
-        }
-    };
 
-	describe("SellersController", function() {
 
-        beforeEach(inject(function ($rootScope, AppResource, centrisNotify, SellerDlg) {
-        	scope = $rootScope.$new();
-            appResource = AppResource;
-            sellerDlg = SellerDlg;
+    beforeEach(inject(function ($controller, $rootScope, AppResource, SellerDlg) {
+    	scope = $rootScope.$new();
+        appResource = AppResource;
+        sellerDlg = SellerDlg;
 
-            spyOn(resource, 'getSellers').and.callThrough();
-            spyOn(resource, 'addSeller').and.callThrough();
-            spyOn(resource, 'updateSeller').and.callThrough();
-            spyOn(sellerDlg, 'show').and.callThrough();
-            spyOn(mockLocation, "path");
+        spyOn(resource, 'getSellers').and.callThrough();
+        spyOn(resource, 'addSeller').and.callThrough();
+        spyOn(resource, 'updateSeller').and.callThrough();
+        spyOn(sellerDlg, 'show').and.callThrough();
 
-        	SellersController = $controller('SellersController', {
-                $scope: scope,
-                $location: mocLocation,
-                AppResource: appResource,
-                sellerDlg: sellerDlg
-            });
-        }));
-    });
+    	SellersController = $controller('SellersController', {
+            $scope: scope,
+            AppResource: appResource,
+            sellerDlg: sellerDlg
+        });
+    }));
+  
 
     it("says SellersController is defined", function() {
         expect(SellersController).toBeDefined();
     });
 
-    it('says testVar is ', function () {
-        expect(scope.testVar).toEqual("sjomli");
-    });
 
 });
